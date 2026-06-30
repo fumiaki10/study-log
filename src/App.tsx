704283712227891fe5bc104c6c73aec3da599a20
fumiaki10 <72801321+fumiaki10@users.noreply.github.com>
2026-06-30 19:52:28 +0900

    カウントを上げ下げするボタンを実装
    ボタンを押すたび１００ずつカウントが上がる
    押した回数分が上がるカウント
    ボタンを押すたび１００ずつ下がる
    ０以下にはならない

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,46 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [pressCount, setPressCount] = useState(0)
+
+  const handlePlus = () => {
+    setCount(count + 100)
+    setPressCount(pressCount + 1)
+  }
+
+  const handleMinus = () => {
+    const nextCount = count - 100
+    if (nextCount < 0) {
+      setCount(0)
+    } else {
+      setCount(nextCount)
+    }
+
+    setPressCount(pressCount + 1)
+  }
 
   return (
     <div>
       <h2>カウントを上げていくボタン</h2>
       <div className="button-counter">
-        <button className="button" onClick={() => setCount(count + 100)}>
-          ボタン
+        <button
+          className="plus-button"
+          onClick={
+            handlePlus
+          }>
+          +ボタン
+        </button>
+        <button
+          className="minus-button"
+          onClick={
+            handleMinus
+          }
+        >
+          -ボタン
+
         </button>
         <div className="counter">{count}</div>
+        <div className="pressCount">{pressCount}</div>
       </div>
 
     </div>
