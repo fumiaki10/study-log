11bc7c07f39dfeaca16a168ff9e7eac7a04d3ed3
fumiaki10 <fumiaki.1225.mw.0429@gmail.com>
2026-07-17 20:17:00 +0900

    feat:処理の共通化、イベント移譲

--- note/SOZO/JS/main.js
+++ note/SOZO/JS/main.js
@@ -1,34 +1,56 @@
-window.addEventListener('load', function () {
-  const ItemButton = document.getElementById("flex-button id1")
-  const ItemButton2 = document.getElementById("flex-button id2")
-  const ItemButton3 = document.getElementById("flex-button id3")
-  const ItemButton4 = document.getElementById("flex-button id4")
-  const ItemButton5 = document.getElementById("flex-button id5")
+function FlexButtons() {
+  const ItemButton = document.getElementById("flex-button1")
+  const ItemButton2 = document.getElementById("flex-button2")
+  const ItemButton3 = document.getElementById("flex-button3")
+  const ItemButton4 = document.getElementById("flex-button4")
+  const ItemButton5 = document.getElementById("flex-button5")
   const FlexItems = document.getElementById("flex-container")
-
-  ItemButton.addEventListener('click', function () {
+  const FlexRemoveButtons = function () {
     FlexItems.classList.remove(...FlexItems.classList)
-    FlexItems.classList.add("class", "btn-primary")
+  }
 
-  })
-  ItemButton2.addEventListener('click', function () {
-    FlexItems.classList.remove(...FlexItems.classList)
-    FlexItems.setAttribute("class", "btn-primary2")
+  function showItem(e) {
+    if (e.target.nodeName === 'INPUT') {
+      FlexRemoveButtons()
+      if (e.target.id === "flex_button1")
+        FlexItems.classList.add("class", "btn-primary")
+      if (e.target.id === "flex_button1")
+        FlexItems.classList.add("class", "btn-primary2")
+      if (e.target.id === "flex_button1")
+        FlexItems.classList.add("class", "btn-primary3")
+      if (e.target.id === "flex_button1")
+        FlexItems.classList.add("class", "btn-primary4")
+      if (e.target.id === "flex_button1")
+        FlexItems.classList.add("class", "btn-primary5")
+    }
+  }
 
-  })
-  ItemButton3.addEventListener('click', function () {
-    FlexItems.classList.remove(...FlexItems.classList)
-    FlexItems.setAttribute("class", "btn-primary3")
 
-  })
-  ItemButton4.addEventListener('click', function () {
-    FlexItems.classList.remove(...FlexItems.classList)
-    FlexItems.setAttribute("class", "btn-primary4")
+  let flexButton = document.getElementById('buttons');
+  flexButton.addEventListener('click', e => showItem(e))
 
-  })
-  ItemButton5.addEventListener('click', function () {
-    FlexItems.classList.remove(...FlexItems.classList)
-    FlexItems.setAttribute("class", "btn-primary5")
 
-  })
-})
+
+  // ItemButton.addEventListener('click', function () {
+  //   FlexRemoveButtons()
+  //   FlexItems.classList.add("class", "btn-primary")
+  // })
+  // ItemButton2.addEventListener('click', function () {
+  //   FlexRemoveButtons()
+  //   FlexItems.classList.add("class", "btn-primary2")
+  // })
+  // ItemButton3.addEventListener('click', function () {
+  //   FlexRemoveButtons()
+  //   FlexItems.classList.add("class", "btn-primary3")
+  // })
+  // ItemButton4.addEventListener('click', function () {
+  //   FlexRemoveButtons()
+  //   FlexItems.classList.add("class", "btn-primary4")
+  // })
+  // ItemButton5.addEventListener('click', function () {
+  //   FlexRemoveButtons()
+  //   FlexItems.classList.add("class", "btn-primary5")
+  // })
+}
+
+window.addEventListener('load', FlexButtons)
