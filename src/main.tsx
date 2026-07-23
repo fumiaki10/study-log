a1369b032ac591a4212c84ed2b4c2106f0f9a6a5
fumiaki10 <fumiaki.1225.mw.0429@gmail.com>
2026-07-23 11:40:22 +0900

    docs:現在使用していないコードのコメントアウト

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import RouterApp from './routerApp.tsx'
+// import RouterApp from './routerApp.tsx'
 import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterApp />
+    {/* <RouterApp /> */}
     <App />
   </StrictMode>,
 )
